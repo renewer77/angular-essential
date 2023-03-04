13869b246fb1cca4cbcb5e6a10d1b980961c0c90
smalishev <smalishev@salsalabs.com>
2023-03-04 17:01:06 -0500

    added files

---
+++ src/app/app.routings.ts
@@ -0,0 +1,20 @@
+import { Routes, RouterModule } from "@angular/router";
+import { MediaItemListComponent } from "./media-item-list.component";
+
+const appRoutes: Routes= [
+    {
+      path: 'add',
+      loadChildren: ()=>import('./new-item/new-item.module').then(mod => mod.NewItemModule)
+    },
+    {
+       path: ':medium',
+       component: MediaItemListComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'all',
+        pathMatch: 'full'
+    }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
